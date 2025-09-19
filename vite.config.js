37f1463041d4647cc89b462c1a3e6853f0d395c1
Maximilian Kuschewski <maxi.kuschewski@gmail.com>
2025-09-19 13:17:12 +0200

    Fix build

--- vite.config.js
+++ vite.config.js
@@ -4,10 +4,5 @@ export default defineConfig({
   base: "/cloudspecs/",
   build: {
     target: "esnext", // Needed so that build can occur with the top-level 'await' statements,
-    rollupOptions: {
-      input: {
-        main: 'main.js'
-      }
-    }
   },
 })
