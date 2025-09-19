f4de6a98aeddb9d0ac97f4863c7c321b8ddacb7d
Maximilian Kuschewski <maxi.kuschewski@gmail.com>
2025-09-19 13:22:32 +0200

    Remove cloudspecs base path

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,6 @@
 import { defineConfig } from 'vite'
 
 export default defineConfig({
-  base: "/cloudspecs/",
   build: {
     target: "esnext", // Needed so that build can occur with the top-level 'await' statements,
   },
