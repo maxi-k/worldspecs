41d5787cd36801bcfc20b5f842eaa9cb47589a53
Maximilian Kuschewski <maxi.kuschewski@gmail.com>
2025-09-19 11:19:13 +0200

    Load duckdb from jsdelivr

--- components/db.js
+++ components/db.js
@@ -1,24 +1,9 @@
 import * as duckdb from '@duckdb/duckdb-wasm';
-import duckdb_wasm from '@duckdb/duckdb-wasm/dist/duckdb-mvp.wasm?url';
-import mvp_worker from '@duckdb/duckdb-wasm/dist/duckdb-browser-mvp.worker.js?url';
-import duckdb_wasm_next from '@duckdb/duckdb-wasm/dist/duckdb-eh.wasm?url';
-import eh_worker from '@duckdb/duckdb-wasm/dist/duckdb-browser-eh.worker.js?url';
 
 // Load static DuckDB database from GitHub
 import dbfile from '/static/cloudspecs.duckdb?url';
-
-const MANUAL_BUNDLES = Object.freeze({
-    mvp: {
-        mainModule: duckdb_wasm,
-        mainWorker: mvp_worker,
-    },
-    eh: {
-        mainModule: duckdb_wasm_next,
-        mainWorker: eh_worker
-    },
-});
-
 const DB_NAME = "cloudspecs.duckdb";
+
 export default class DB {
   #db; #conn;
 
@@ -28,11 +13,15 @@ export default class DB {
   }
 
   static async create() {
-    // Select a bundle based on browser checks
-    const bundle = await duckdb.selectBundle(MANUAL_BUNDLES);
+    // Load duckdb wasm from jsdelivr
+    const JSDELIVR_BUNDLES = duckdb.getJsDelivrBundles();
+    const bundle = await duckdb.selectBundle(JSDELIVR_BUNDLES);
+    const worker_url = URL.createObjectURL(
+      new Blob([`importScripts("${bundle.mainWorker}");`], {type: 'text/javascript'})
+    );
 
     // Instantiate the asynchronus version of DuckDB-wasm
-    const worker = new Worker(bundle.mainWorker);
+    const worker = new Worker(worker_url);
     const logger = new duckdb.ConsoleLogger();
     const db = new duckdb.AsyncDuckDB(logger, worker);
     await db.instantiate(bundle.mainModule, bundle.pthreadWorker);
